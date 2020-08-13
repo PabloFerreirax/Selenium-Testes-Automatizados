306ccfa5d0ed289d06b7b079b5eeed8c9c4dfd32
PabloFerreirax <pablo.estudoss@gmail.com>
2020-08-13 11:12:06 -0300

    Metodo ResumoMensal vazio adicionado

--- src/main/java/com/selenium/basic/teste/ResumoTeste.java
+++ src/main/java/com/selenium/basic/teste/ResumoTeste.java
@@ -8,6 +8,11 @@ import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static com.selenium.basic.core.DriverFactory.getDriver;
 
@@ -25,10 +30,22 @@ public class ResumoTeste extends BaseTeste {
         Assert.assertEquals("Movimentação removida com sucesso!", resumoPage.obterTesteSucesso());
     }
 
+    //@Test(expected = NoSuchElementException.class) //Metodo 1
     @Test
     public void test2ResumoMensal(){
         menuPage.acessarTelaResumo();
         Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+        /*try{ //metodo 2
+            DriverFactory.getDriver().findElement(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+            Assert.fail();
+        }
+        catch (NoSuchElementException e){
+
+        }*/
+        // Metodo 3
+        List<WebElement> elementosEncontrados =
+                DriverFactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+        Assert.assertEquals(0,elementosEncontrados.size());
     }
 
 }
